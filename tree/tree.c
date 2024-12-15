584d84c8dd359cf3ba686728f63e1b1978b1022c
Tian <xtyao2015@gmail.com>
2024-12-15 14:30:38 -0500

    add create node function

--- tree/tree.c
+++ tree/tree.c
@@ -8,6 +8,29 @@ Tree root = { .n = {
 	.path = "/"	
 }};
 
+void zero(int8 *str, int16 size) {
+	int8 *p;
+	int16 n;
+	
+	for (n = 0, p = str; n < size; ++p; ++n) *p = 0;
+	return;
+}
+
+Node *createNode(Node *parent, int8 *path) {
+	Node *n;
+	int16 size;
+	assert(parent);
+	size = sizeof(struct s_node);
+	n = (Node *)malloc(size);
+	zero((int8 *)n, size);
+	
+	parent->west = n;
+	n->tag = TagNode;
+	n->north = parent;
+	strncpy((char *)n->path, (char *)path, 255);
+	return n;
+}
+
 int main() {
 	printf("%p\n", (void *)&root);
 	return 0;
