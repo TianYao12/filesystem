82dfacfa1c2f03a63122ee7537ad85dab730e255
Tian <xtyao2015@gmail.com>
2024-12-15 09:59:18 -0500

    add structs, clean makefile

--- tree/tree.c
+++ tree/tree.c
@@ -1,6 +1,14 @@
 #include "tree.h"
 
+Tree root = { .n = {
+	.tag = (TagRoot | TagNode),
+	.north = (Node *)&root,
+	.west = 0,
+	.east = 0,
+	.path = "/"	
+}};
+
 int main() {
-	printf("Hello word\n");
+	printf("%p\n", (void *)&root);
 	return 0;
 }
